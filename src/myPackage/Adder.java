ee88a6db25c7ad752e7ada924b0fdfdd5834a05b
cheile01 <cheile01@dcs.bbk.ac.uk>
2014-10-14 19:22:19 +0100

    class created

---
+++ src/myPackage/Adder.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class Adder {
+
+	public int add(int input1, int input2) {
+		return input1 + input2;
+	}
+
+}
--- src/myPackage/AdderTest.java
+++ src/myPackage/AdderTest.java
@@ -17,7 +17,7 @@ public class AdderTest {
 	public void test() {
 		int input1 = 3;
 		int input2 = 5;
-		int expectedOutput = 9;
+		int expectedOutput = 8;
 
 		Adder addObj = new Adder();
 
@@ -26,5 +26,20 @@ public class AdderTest {
 		assertEquals("Wrong answer!", expectedOutput, actualOutput);
 
 	}
+	
+	@Test
+	public void test2() {
+		int input1 = 7;
+		int input2 = 4;
+		int expectedOutput = 11;
+
+		Adder addObj = new Adder();
+
+		int actualOutput = 	addObj.add(input1,input2);
+
+		assertEquals("Wrong answer!", expectedOutput, actualOutput);
+
+	}
+
 
 }
